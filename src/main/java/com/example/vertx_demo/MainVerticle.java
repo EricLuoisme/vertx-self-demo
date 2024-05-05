70eccfef806e433ac671d546dfa80f94e6de9aa7
EricLuoisme <35546172+EricLuoisme@users.noreply.github.com>
2024-05-05 11:13:54 +0800

    chore: add router & handler logic under Vertx framework

--- src/main/java/com/example/vertx_demo/MainVerticle.java
+++ src/main/java/com/example/vertx_demo/MainVerticle.java
@@ -1,12 +1,38 @@
 package com.example.vertx_demo;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.MultiMap;
 import io.vertx.core.Promise;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
+
+    // 创建路由
+    Router router = Router.router(vertx);
+
+    // 嵌入处理逻辑
+    router.route().handler(context -> {
+
+      // 获取请求相关信息
+      String address = context.request().connection().remoteAddress().toString();
+      MultiMap queryParams = context.queryParams();
+      String name = queryParams.contains("name") ? queryParams.get("name") : "unknown";
+
+      // 响应内容
+      context.json(
+        new JsonObject()
+          .put("name", name)
+          .put("address", address)
+          .put("message", "Hello " + name + " connected from " + address)
+      );
+
+    });
+
+
     vertx.createHttpServer().requestHandler(req -> {
       req.response()
         .putHeader("content-type", "text/plain")
